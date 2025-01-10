9b2c00c575ff05a2491f3d065df10cf1cb418316
Keiyo Nakayama <keiyou0206@gmail.com>
2025-01-10 15:17:38 +0900

    fix

--- test/test.bash
+++ test/test.bash
@@ -1,14 +1,18 @@
 #!/bin/bash -xv
-#SPDX-FileCopyrightText: 2025 Keiyo Nakayama
+# SPDX-FileCopyrightText: 2025 Keiyo Nakayama
 # SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-colcon build
-source $dir/.bashrc
-timeout 10 ros2 launch mypkg cpustats.launch.py > /tmp/mypkg.log
 
- 
+
+colcon build
+
+source $dir/.bashrc
+
+timeout 30 ros2 launch mypkg cpustats.launch.py > /tmp/mypkg.log
+
 cat /tmp/mypkg.log | grep -E 'Context Switches:.*, Interrupts:.*, Soft Interrupts:.*, Syscall:.*'
+
